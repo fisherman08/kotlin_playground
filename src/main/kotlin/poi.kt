adf3a41fcd394365a4b8c9f3e2d6a912de122d50
Y K <orenoikizama@hotmail.com>
2018-02-28 09:02:44 +0900

    poiで結合したセルの値を取得

--- src/main/kotlin/poi.kt
+++ src/main/kotlin/poi.kt
@@ -2,6 +2,7 @@
 import java.io.FileInputStream
 import java.nio.file.Paths
 import org.apache.poi.ss.usermodel.*
+import org.apache.poi.ss.util.CellRangeAddressBase
 
 fun main(args :Array<String>){
 
@@ -11,14 +12,14 @@ fun main(args :Array<String>){
 
         workbook = WorkbookFactory.create(FileInputStream(Paths.get(String.javaClass.getResource("/poi/test.xlsx").toURI()).toFile()))
 
-        val sheet :Sheet? = workbook["新シート"]
+        val sheet :Sheet? = workbook["結合テスト"]
 
         if(sheet != null){
-            val cell :Cell? = sheet[2, 2]
-            println(cell?.numericCellValue?.toInt())
+            for(i in 0..4) {
+                println(sheet[i, 0])
+            }
         }
-
-
+        
     } catch (e: Exception){
         e.printStackTrace()
     } finally {
@@ -33,5 +34,16 @@ operator fun Workbook.get(sheetName :String) :Sheet? {
 }
 
 operator fun Sheet.get(row :Int, column :Int) :Cell? {
-    return this.getRow(row)?.getCell(column)
+    val cell = this.getRow(row)?.getCell(column) ?: return null
+
+    for(mergedRegion in this.mergedRegions) {
+        if(mergedRegion.isInRange(cell) &&  !mergedRegion.isFirstCell(cell)) {
+            return this[mergedRegion.firstRow, mergedRegion.firstColumn]
+        }
+    }
+    return cell
+}
+
+fun CellRangeAddressBase.isFirstCell(cell :Cell) :Boolean {
+    return(cell.rowIndex == this.firstRow && cell.columnIndex == this.firstColumn)
 }
