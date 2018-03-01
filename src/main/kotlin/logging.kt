81a0a3597eb3ad1a1a23d78cd9b5cc0913e55e1a
Y K <orenoikizama@hotmail.com>
2018-03-01 15:00:25 +0900

    log4j

--- src/main/kotlin/logging.kt
+++ src/main/kotlin/logging.kt
@@ -1,28 +1,40 @@
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.core.LoggerContext
-import org.apache.logging.log4j.core.config.LoggerConfig
-import javax.security.auth.login.Configuration.getConfiguration
-
 
 
 fun main(args :Array<String>){
+    System.setProperty("logDir", "log3")
+    System.setProperty("logFile", "hogege")
+
+    val loggerContext = LogManager.getContext(false) as LoggerContext
+    val configuration = loggerContext.getConfiguration()
+    val loggerConfig = configuration.getLoggerConfig(LogManager.ROOT_LOGGER_NAME)
+
+    if(args.contains("-level=info")) {
+        loggerConfig.level = Level.INFO
+    } else {
+        loggerConfig.level = Level.ERROR
+    }
+
+
+    loggerContext.updateLoggers()
+
+    val logger = LogManager.getLogger("kotlin_playground")
 
     try {
 
-        val loggerContext = LogManager.getContext(false) as LoggerContext
-        val configuration = loggerContext.getConfiguration()
-        val loggerConfig = configuration.getLoggerConfig(LogManager.ROOT_LOGGER_NAME)
-
-        loggerConfig.setLevel(Level.INFO)
-        loggerContext.updateLoggers()
-        val logger = LogManager.getLogger("kotlin_playground")
-
-
         logger.info("info")
-        logger.error("error")
+        logger.error("エラーが発生しました！")
+
+        throw Exception("だめだkりゃ")
+
     } catch (e: Exception){
-        e.printStackTrace()
+        var message = e.toString()
+        for(st in e.stackTrace) {
+            message = "$message\nat ${st}"
+        }
+       logger?.error(message)
     }
 
 
