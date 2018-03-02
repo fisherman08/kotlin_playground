4b112216bd410cd1ba3c7606f86d4ab7db10b297
Y K <orenoikizama@hotmail.com>
2018-03-02 09:34:23 +0900

    httpclient

---
+++ src/main/kotlin/http.kt
@@ -0,0 +1,27 @@
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.gson.responseObject
+import com.github.kittinunf.fuel.httpGet
+
+fun main(args: Array<String>){
+
+    try {
+
+        FuelManager.instance.basePath = "https://www.google.co.jp"
+
+        // Stringで撮ってくるバージョン
+        val (request, response, result) = "/".httpGet().responseString(charset = Charsets.UTF_8)
+
+        // Gsonで取得するバージョン
+        //data class HttpBinUserAgentModel(var userAgent: String = "")
+        //val (request, response, result) = "/".httpGet().responseObject<HttpBinUserAgentModel>()
+        //Fuel.get("/").responseObject<HttpBinUserAgentModel> { _, _, result ->
+        //}
+
+        val (d, err) = result
+        println(d)
+    } catch(e :Exception) {
+        e.printStackTrace()
+    }
+
+}
